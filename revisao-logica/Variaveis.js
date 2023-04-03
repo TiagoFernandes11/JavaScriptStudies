d736d4864f6f67b1965026cc483d9ff9ba3e4c9e
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 10:39:45 -0300

    Initial commit

---
+++ revisao-logica/Variaveis.js
@@ -0,0 +1,10 @@
+// var, let, const
+
+// var -> ES5 - cross-browser
+//let e const -> ES2015 (só nao funciona no IE, ou seja, funciona em todos)
+
+let teste = "minha string";
+console.log(teste);
+// exemplo de tipagem fraca
+teste = 10;
+console.log(teste);
