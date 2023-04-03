2a87e3f95d48650a10f0a7e4b3e622b167f62b23
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 11:46:45 -0300

    Create Condicional-If-e-Else.js

---
+++ revisao-logica/Condicional-If-e-Else.js
@@ -0,0 +1,7 @@
+if(false){
+    console.log("Executado o primeiro if");
+}else if(false){
+    console.log("Isso nao será executado");
+}else{
+    console.log("Tambem não sera(Apenas se o primeiro if ser falso)");
+}
