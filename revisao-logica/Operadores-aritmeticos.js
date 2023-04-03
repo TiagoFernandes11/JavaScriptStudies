0e483a1efaffe65d90e31a51007fd2982e62a42f
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 11:30:02 -0300

    Update Operadores-aritmeticos.js

---
+++ revisao-logica/Operadores-aritmeticos.js
@@ -0,0 +1,16 @@
+//soma +
+//subtração -
+//divisao /
+//multiplicacao *
+//resto(modulo) %
+//potencia **
+
+let n1 = 10;
+let n2 = 5;
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 / n2);
+console.log(n1 * n2);
+console.log(n1 % n2);
+console.log(n1 ** n2);
