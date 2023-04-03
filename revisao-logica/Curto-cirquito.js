9ee4f574753045df967534083384b45069755996
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 12:04:28 -0300

    Update Curto-cirquito.js

--- revisao-logica/Curto-cirquito.js
+++ revisao-logica/Curto-cirquito.js
@@ -1,3 +1,7 @@
 0 || console.log("executou?");
 
 1 && console.log("executou de novo ?");
+
+console.log("executou?") || console.log("executou de novo?");
+
+console.log("executou?") && console.log("executou de novo ?");
