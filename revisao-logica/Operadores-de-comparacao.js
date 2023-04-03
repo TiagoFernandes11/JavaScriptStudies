170db6b502856bd3d6308d215c9b47661e1668d1
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 11:39:02 -0300

    Create Operadores-de-comparacao.js

---
+++ revisao-logica/Operadores-de-comparacao.js
@@ -0,0 +1,11 @@
+//igualdade de valores ==
+//igualdade de valores e tipos ===
+
+//maior que >
+//menor que <
+
+//maior ou igual =>
+//menor ou igual =<
+
+//diferente em valor !=
+//diferente em valor e tipo !==
