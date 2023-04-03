576935b7590646263574169e833472f89f8b40f2
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 11:15:31 -0300

    Update conversao-de-tipos.js

--- revisao-logica/conversao-de-tipos.js
+++ revisao-logica/conversao-de-tipos.js
@@ -1,6 +1,7 @@
 let n1 = 10;
 let n2 = "10";
 
+//Conversões implicitas
 console.log(n1 * n2);
 console.log(typeof n1, typeof n2);
 
@@ -9,3 +10,10 @@ console.log(n1 === n2);
 
 console.log(n1 + n2);
 console.log(typeof(n1 + n2));
+
+//-------------------------------------------------------------------------//
+//conversões explicitas
+
+
+// parseInt, parseFloat, Number()
+console.log(n1 + parseInt(n2));
