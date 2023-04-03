0b347bd81b5095c5f3c206e81daa4bcfec483a1a
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-03 11:54:55 -0300

    Create Falsy-And-Truely-values.js

---
+++ revisao-logica/Falsy-And-Truely-values.js
@@ -0,0 +1,52 @@
+//falsy values - 0, "", NaN, undefined, null, false
+//truth - todos os demais
+
+let valor = 10;
+
+if(valor){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+valor = 0;
+
+if(valor){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+valor = "";
+
+if(valor){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+valor = "alguma coisa";
+
+if(valor){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+valor = NaN;
+
+if(valor){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+valor = undefined;
+
+if(valor){
+    console.log("true");
+} else {
+    console.log("false");
+}
