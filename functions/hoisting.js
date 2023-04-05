a4f2c38a9c20e8126d50c2a54b826d94fe663959
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-05 11:56:08 -0300

    Create hoisting.js

---
+++ functions/hoisting.js
@@ -0,0 +1,17 @@
+teste();
+
+function teste(){
+    console.log("teste");
+}
+
+console.log("Após a funçao: ");
+
+teste();
+
+//funcao(); gerou erro
+
+const funcao = function teste(){
+    console.log("teste");
+}
+
+funcao();
