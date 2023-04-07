1d0518bf1660f948b6e7ecdbe39f246547328aa3
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-07 16:19:46 -0300

    Update script.js

--- dom/exemplo1/script.js
+++ dom/exemplo1/script.js
@@ -1,4 +1,7 @@
-document.getElementsByClassName("paragrafo2")[0].textContent = "teste1"
+document.getElementsByClassName("paragrafo2").textContent = "teste1"
 document.getElementsByClassName("paragrafo2")[0].textContent = "teste2"
 
 //testes realizados diretamente no console do google chrome
+
+document.querySelectorAll("h1").textContent ="teste teste"
+document.querySelectorAll("h1")[0].textContent ="teste teste"
