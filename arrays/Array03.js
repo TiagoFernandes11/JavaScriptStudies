b2a638806b6538537704dbb2f481e4027f0603fb
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-10 15:41:53 -0300

    Create Array03.js

---
+++ arrays/Array03.js
@@ -0,0 +1,12 @@
+console.clear();
+
+
+let arr1 = [1,2,3];
+let arr2 = [5,6,7];
+
+console.log(typeof arr1.toString() , arr1.toString());
+
+console.log(arr1.join(" - "));
+
+console.log(arr1.concat(4, arr2));
+
