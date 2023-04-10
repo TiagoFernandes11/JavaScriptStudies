a2d0cccc03de87e2ca339c5fb88dbd5e953f53ba
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-09 22:56:02 -0300

    Recuperando um input

--- dom/DOM_newsletter_original/js/saudacao.js
+++ dom/DOM_newsletter_original/js/saudacao.js
@@ -1,4 +1,9 @@
-(function () {
+const msgFeedback = document.getElementById("newsletterFeedback");
 
+//recuperar os valores no momento certo
 
-})()
+//trigger dessa função está no evento onClick do botão
+function cadastrarEmail(){
+    let emailInput = document.getElementById("txtEmail"); // pega o elemento
+    msgFeedback.innerHTML = `O email ${emailInput.value} foi cadastrado com sucesso`;
+}
