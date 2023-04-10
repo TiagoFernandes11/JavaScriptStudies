f238cd4ced3aed9e8af80791d0d2c7e14e2d9072
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-10 14:25:56 -0300

    Update Array01.js

--- arrays/Array01.js
+++ arrays/Array01.js
@@ -5,9 +5,22 @@ let soNumeros = arr.every( function (elemento) {
     return typeof elemento === "number";
 });
 
+// verificando se alguns dos elementos sao numeros
 let algunsNumeros = arr.some(function(elemento){
     return typeof elemento === "number";
 });
 
+//filtrando os numeros do array
+const arr1 = arr.filter(function(elemento, i, functionArray){
+    return typeof elemento === "number";
+})
+
+//iterando array
+arr.forEach(function(elemento, i){
+    console.log(i," : ",elemento)
+})
+
 console.log("Every number ? ", soNumeros)
 console.log("Some numeros? ",algunsNumeros);
+console.log("Filter number: ",arr1)
+console.log("Original array: ", arr)
