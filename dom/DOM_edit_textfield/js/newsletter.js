72b9e3a6883261ff41604ea56be648057f88792d
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-09 23:15:36 -0300

    implementaçao do bagui bugado

--- dom/DOM_edit_textfield/js/newsletter.js
+++ dom/DOM_edit_textfield/js/newsletter.js
@@ -1,4 +1,13 @@
-const txtEmail = document.getElementById("txtEmail")
+const txtEmail = document.getElementById("txtEmail");
+
+function editarEmail(){
+    txtEmail.disabled = false;
+    txtEmail.focus();
+}
+
+function blurEmail(){
+    txtEmail.disabled = true;
+}
 
 
 
