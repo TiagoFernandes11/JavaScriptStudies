a7e7160bcfa7e50077c3b2082c1f01b04786d47b
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-11 12:06:23 -0300

    Update Desafio.js

--- arrays/Desafio.js
+++ arrays/Desafio.js
@@ -1,11 +1,13 @@
 function soma(){
-    let result = 0;
-    for(elementos in arguments){
-        result += elementos;
-    }
-    return result;
+    const numbers = [...arguments];
+    return numbers.reduce(function(soma, atual){
+        return soma + atual;
+    }, 0)
 };
 
-function media(){
-    return soma(arguments)/arguments.length;
-};
+function average(){
+    return soma(...arguments)/ arguments.length;
+}
+
+console.log(soma(1,2,3,4,5));
+console.log(average(1,2,3,4,5));
