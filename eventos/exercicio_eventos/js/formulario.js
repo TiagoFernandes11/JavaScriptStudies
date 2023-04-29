bb14046c930c6e5f43bfa42de8202a3f4313e802
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-29 13:16:47 -0300

    Exercicio que não funciona só Deus sabe porque

---
+++ eventos/exercicio_eventos/js/formulario.js
@@ -0,0 +1,36 @@
+(function (){
+    "use strict";
+    const titulo = document.getElementById("txtTitulo");
+    const descricao = document.getElementById("txtDescricao");
+    const contadorContainer = document.getElementById("contador");
+    const resta = contadorContainer.getElementsByTagName("span")[0];
+    const maxima = descricao.maxlength;
+    const formCadastro = document.querySelector(".formCadastro");
+
+    formCadastro.addEventListener("submit", function (e){
+        if(!titulo.value){
+            alert("Preencha todos os campos!");
+            e.preventDefault();
+            titulo.focus();
+        }
+        if(!descricao.value){
+            alert("Preencha todos os campos!");
+            e.preventDefault();
+            descricao.focus();
+        }
+        
+    })
+
+    contadorContainer.removeAttribute("style");
+
+    function checkLenght(){
+
+        let numeroLetrasDigitadas = descricao.value.lenght;
+        let caracteresRestantes = (parseInt(maxima) - parseInt(numeroLetrasDigitadas));
+        resta.textContent = caracteresRestantes;
+
+    }
+
+    descricao.addEventListener("input", checkLenght());
+    
+})();
