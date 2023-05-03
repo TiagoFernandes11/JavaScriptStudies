66480b8b5a76418e04cb6bcc2f46984190b37538
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-05-03 10:45:36 -0300

    Update todolist.js

--- to-do-list/js/todolist.js
+++ to-do-list/js/todolist.js
@@ -3,26 +3,34 @@
     const itemInput = document.getElementById("item-input");
     const todoAddForm = document.getElementById("todo-add");
     const ul = document.getElementById("todo-list");
+    const lis = ul.getElementsByTagName("li");
+
+    function addEventLi(li){
+        li.addEventListener("click", function(e){
+            console.log(this);
+        });
+    }
+
+    function addTask(task){
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.className = "task-name";
+        p.textContent = task;
+        li.className = "todo-item";
+        li.appendChild(p);
+        ul.appendChild(li);
+
+        addEventLi(li);
+    };
 
     todoAddForm.addEventListener("submit", function(e){
         e.preventDefault();
-        console.log(itemInput.value);
-        ul.innerHTML += `
-        <li class="todo-item">
-        <button class="button-check">
-            <i class="fas fa-check displayNone"></i>
-        </button>
-        <p class="task-name">${itemInput.value}</p>
-        <i class="fas fa-edit"></i>
-        <!-- <div class="editContainer">
-            <input class="editInput" type="text">
-            <button class="editButton">Edit</button>
-            <button class="cancelButton">Cancel</button>
-        </div> -->
-        <i class="fas fa-trash-alt"></i>
-    </li>`;
+        addTask(itemInput.value);
+        itemInput.value = "";
+        itemInput.focus();
+    });
 
-    itemInput.value = "";
-    itemInput.focus();
+    [...lis].forEach(li => {
+       addEventLi(li);
     });
 })();
