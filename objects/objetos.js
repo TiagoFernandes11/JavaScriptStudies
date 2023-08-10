0efd35bcc04d7da29f9d9f453079fe11fb68688a
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-08-10 10:58:49 -0300

    Create objetos.js

---
+++ objects/objetos.js
@@ -0,0 +1,14 @@
+const obj1 = {
+    nome: "Tiago"
+};
+
+const obj2 = new Object();
+
+obj2.nome = "Tiago";
+obj2["idade"] = "24";
+
+console.log(obj1);
+console.log(obj2);
+
+const str = "minha string";
+console.log(str.length);
