21441dc30327f43dc950248330af8daa72c70763
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-10-14 10:55:10 -0300

    Create contructors.js

---
+++ objects/contructors.js
@@ -0,0 +1,19 @@
+function Task(name){
+    let _name = name;
+    this.createdAt = new Date()
+    this.changeName = function(newName){
+        _name = newName;
+    }
+    this.getName = function(){
+        return _name;
+    }
+};
+
+
+const task1 = new Task();
+const task2 = new Task("chora nene");
+
+console.log(task1);
+console.log(task2);
+task1.changeName("pinto");
+console.log(task1.getName());
