ff374a30b52ce97c955c3bc067540860d7e5f61b
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-10-22 19:00:04 -0300

    mais algumas aulas

---
+++ estilizacao/Introducao.js
@@ -0,0 +1,5 @@
+//background dinamico, destaque de botões e etc
+
+//não assisti a maioria das aulas (conteudo chatinho)
+
+//caso precise assistir essas aulas, seu safado!
---
+++ functionsPt2/ArrowFunctions.js
@@ -0,0 +1,1 @@
+// não entendi o proposito dessa aula, não aprendi nada novo kk
