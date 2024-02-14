9a2f9754a31a0fee96bff91101ecbf22ca0713b3
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-02-14 12:25:08 -0300

    A lot of changes haha

--- mvc-project/js/views/EditAluno.view.js
+++ mvc-project/js/views/EditAluno.view.js
@@ -1,29 +1,34 @@
 class EditAlunoView {
-  constructor() {
-    this.render();
+  constructor(container, materias) {
+    this.container = container;
+    this.meterias = materias;
   }
 
   render(aluno) {
-    const html = `
-        <div class="row">
-        <div class="input-field col s4">
-          <input id="materia" type="text" class="validate" />
-        </div>
-        <div class="input-field col s2">
-          <input id="nota_materia_1" type="text" class="validate" />
-        </div>
-        <div class="input-field col s2">
-          <input id="nota_materia_2" type="text" class="validate" />
-        </div>
-        <div class="input-field col s2">
-          <input id="nota_materia_3" type="text" class="validate" />
-        </div>
-        <div class="input-field col s2">
-          <input id="nota_materia_4" type="text" class="validate" />
-        </div>
-      </div>
-        `;
+    const html = this.meterias
+      .map((materia) => {
+        return `
+          <div class="row">
 
-    document.querySelector("[data-edit-notas]").innerHTML = html;
+          <div class="input-field col s4">
+            <input id="materia_${materia}" type="text" class="validate" disabled value="${materia}"/>
+          </div>
+          <div class="input-field col s2">
+            <input id="nota_materia_1" type="text" class="validate" />
+          </div>
+          <div class="input-field col s2">
+            <input id="nota_materia_2" type="text" class="validate" />
+          </div>
+          <div class="input-field col s2">
+            <input id="nota_materia_3" type="text" class="validate" />
+          </div>
+          <div class="input-field col s2">
+            <input id="nota_materia_4" type="text" class="validate" />
+          </div>
+        </div>
+          `;
+      })
+      .join("");
+    this.container.innerHTML = html;
   }
 }
